c5445b4c163c4dc7da5e57c9aba07403cfa4a7dd
Nicolo Massari <nmassari@ethz.ch>
2025-04-24 15:40:16 +0200

    fix: hf_token as env variable

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,21 +1,14 @@
-import configparser
 import os
 
-import pytest
 from huggingface_hub import login
 
 from ghe_transcribe.core import transcribe
 
 # Assume your config.json is in the root of your project for testing
-CONFIG_PATH = "config.ini"
 TEST_AUDIO_PATH = "media/testing_audio_01.mp3"
 
-@pytest.fixture(scope="session", autouse=True)
-def login_huggingface():
-    """Logs into Hugging Face Hub for the entire test session."""
-    config = configparser.ConfigParser()
-    config.read(CONFIG_PATH)
-    login(token=config["HF_TOKEN"])
+HF_TOKEN = os.environ.get("HF_TOKEN")
+login(token=HF_TOKEN)
 
 def test_transcribe_snippet():
     """Tests the transcribe function with a snippet and speaker count."""
@@ -27,8 +20,6 @@ def test_transcribe_snippet():
 
 # Clean up dummy files after testing (optional)
 def teardown_module():
-    if os.path.exists(CONFIG_PATH):
-        os.remove(CONFIG_PATH)
     if os.path.exists(os.path.splitext(TEST_AUDIO_PATH)[0]+".wav"):
         os.remove(os.path.splitext(TEST_AUDIO_PATH)[0]+".wav")
     if os.path.exists(os.path.splitext(TEST_AUDIO_PATH)[0] + "_snippet.wav"):
