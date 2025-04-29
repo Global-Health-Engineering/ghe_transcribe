20981aeafc926c62248b9b055b88f9d381eee17c
Nicolo Massari <nmassari@ethz.ch>
2025-04-29 17:16:06 +0200

    fix: update script.py and tests

--- script.py
+++ script.py
@@ -1,12 +1,3 @@
-import json
-
-from huggingface_hub import login
-
 from ghe_transcribe.core import transcribe
 
-with open("config.json", "r") as f:
-    config = json.load(f)
-
-login(token=config["HF_TOKEN"])
-
-result = transcribe("media/testing_audio_01.mp3", num_speakers=2)
+result = transcribe("media/testing_audio.mp3", trim=5, num_speakers=2)
--- tests/test_core.py
+++ tests/test_core.py
@@ -1,18 +1,15 @@
 import os
 
-from huggingface_hub import login
-
 from ghe_transcribe.core import transcribe
 
 # Assume your config.json is in the root of your project for testing
-TEST_AUDIO_PATH = "media/testing_audio_01.mp3"
+TEST_AUDIO_PATH = "media/testing_audio.mp3"
 
-HF_TOKEN = os.environ.get("HF_TOKEN")
-login(token=HF_TOKEN)
+huggingface_token = os.environ.get("HUGGINGFACE_TOKEN")
 
 def test_transcribe_snippet():
     """Tests the transcribe function with a snippet and speaker count."""
-    result = transcribe(TEST_AUDIO_PATH, snip=5, num_speakers=1, save_output=False, info=False)
+    result = transcribe(TEST_AUDIO_PATH, huggingface_token="huggingface_token", trim=5, num_speakers=1, save_output=False, info=False)
 
     # Add assertions to check the result
     assert isinstance(result, list), "Transcribe should return a list."
