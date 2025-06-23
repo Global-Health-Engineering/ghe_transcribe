40b1c1cdab58a32a2231850107507b0df36d5059
Conventional Changelog Action <conventional.changelog.action@github.com>
2025-06-23 15:10:14 +0000

    chore(release): v0.4.0 [skip ci]

--- build/lib/ghe_transcribe/_version.py
+++ build/lib/ghe_transcribe/_version.py
@@ -17,5 +17,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.3.3.dev2+g381b1d2.d20250620'
-__version_tuple__ = version_tuple = (0, 3, 3, 'dev2', 'g381b1d2.d20250620')
+__version__ = version = '0.3.4.dev8+g729105e.d20250623'
+__version_tuple__ = version_tuple = (0, 3, 4, 'dev8', 'g729105e.d20250623')
--- src/ghe_transcribe/_version.py
+++ src/ghe_transcribe/_version.py
@@ -17,5 +17,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.3.4.dev3+g102c28d.d20250620'
-__version_tuple__ = version_tuple = (0, 3, 4, 'dev3', 'g102c28d.d20250620')
+__version__ = version = '0.3.4.dev8+g729105e.d20250623'
+__version_tuple__ = version_tuple = (0, 3, 4, 'dev8', 'g729105e.d20250623')
